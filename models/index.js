856eb4d284ce689e2c9efbb344cc7c57dd135878
Joma121 <martinsjoshua1986@gmail.com>
2020-12-13 15:14:18 -0800

    refactors index model with mongoose mongodb connection setup

--- models/index.js
+++ models/index.js
@@ -1,3 +1,19 @@
+const mongoose = require("mongoose");
+
+const mongoUrl = "mongodb://localhost:27017/";
+
+mongoose.connect(mongoUrl, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+mongoose.connection.on("connected", () => {console.log("Mongodb Connected...");})
+mongoose.connection.on("disconnected", () => {console.log("Mongodb Disconnected...");})
+mongoose.connection.on("error", (error) => {console.warn("Mongodb Error!", error);})
+
+
 module.exports = {
     Furniture: require("./Furniture")
 }
