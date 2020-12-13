0b04318b7744e0a7cc2a0d3f9ddc77c54ec8c5d1
Joma121 <martinsjoshua1986@gmail.com>
2020-12-13 14:04:59 -0800

    adds new view and route, post route for new furniture

--- controllers/furnitures.js
+++ controllers/furnitures.js
@@ -2,11 +2,26 @@ const express = require('express');
 const router = express.Router();
 const db = require("../models");
 
+/* Index */
 router.get("/", (req, res) => {
     const context = { furnitures: db.Furniture };
     res.render("furnitures/index", context);
 });
 
+
+/* New */
+router.get("/new", (req, res) => {
+    res.render("furnitures/new");
+})
+
+/* Create */
+router.post("/", (req, res) => {
+    console.log("=== Created ===\n", req.body);
+    db.Furniture.push(req.body);
+    res.redirect("/furnitures")
+})
+
+/* Show */
 router.get("/:index", (req, res) => {
     const index = req.params.index;
     const furniture = db.Furniture[index];
