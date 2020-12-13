02ab6091a9028d989a6ebaf586d818f8af879c9e
Joma121 <martinsjoshua1986@gmail.com>
2020-12-13 12:32:47 -0800

    installs ejs, creates files in folder structure, sets up home route

---
+++ server.js
@@ -0,0 +1,32 @@
+/* External Modules */
+const express = require("express");
+const methodOverride = require("method-override");
+
+/* Internal Modules */
+const db = require("./models");
+const controllers = require("./controllers");
+
+/* Instanced Module */
+const app = express();
+
+/* Variables */
+const PORT = 4000;
+
+/* Config */
+app.set("view engine", "ejs");
+
+
+/* Middleware */
+app.use(express.urlencoded({extended: true}));
+
+app.use(methodOverride("_method"));
+
+/* Home Route */
+app.get('/', (request, response) => {
+    response.render("home");
+})
+
+/* Server Bind */
+app.listen(PORT, function () {
+	console.log(`Server is live at http://localhost:${PORT}/`);
+});
