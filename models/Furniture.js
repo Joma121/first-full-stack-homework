23cd65f40a7688835e1608f2bf2b2a7f42a3844d
Joma121 <martinsjoshua1986@gmail.com>
2020-12-13 15:11:24 -0800

    refactors Furniture model with mongoose schema

--- models/Furniture.js
+++ models/Furniture.js
@@ -1,5 +1,23 @@
-module.exports = [
-    { name: "Windelbrooke", finish: "Oak", quantity: 12 },
-    { name: "Laxboi", finish: "Pleather", quantity: 0 },
-    { name: "Agonee", finish: "Mahongy", quantity: 4 }
-]
+const mongoose = require('mongoose');
+
+const furnitureSchema = new mongoose.Schema({
+    name: {type: String, required: true, minlength: 3},
+    finish: String,
+    quantity: Number,
+    },
+    {timestamps: true}
+);
+
+const Furniture = mongoose.model("Furniture", furnitureSchema);
+
+module.exports = Furniture;
+
+
+
+
+
+// module.exports = [
+//     { name: "Windelbrooke", finish: "Oak", quantity: 12 },
+//     { name: "Laxboi", finish: "Pleather", quantity: 0 },
+//     { name: "Agonee", finish: "Mahongy", quantity: 4 }
+// ]
