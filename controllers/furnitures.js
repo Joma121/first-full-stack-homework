b38f9661dfb4d199605a561d2a1591d1da23cc50
Joma121 <martinsjoshua1986@gmail.com>
2020-12-13 13:29:53 -0800

    adds show view and route to see furniture details

--- controllers/furnitures.js
+++ controllers/furnitures.js
@@ -2,9 +2,16 @@ const express = require('express');
 const router = express.Router();
 const db = require("../models");
 
-router.get("/", (request, response) => {
+router.get("/", (req, res) => {
     const context = { furnitures: db.Furniture };
-    response.render("furnitures/index", context);
+    res.render("furnitures/index", context);
 });
+
+router.get("/:index", (req, res) => {
+    const index = req.params.index;
+    const furniture = db.Furniture[index];
+    const context = {furniture: furniture};
+    res.render("furnitures/show", context)
+})
 
 module.exports = router;
